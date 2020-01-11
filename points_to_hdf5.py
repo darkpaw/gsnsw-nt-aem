531e148c1d68ec3e03c4568c7b301568dda85096
felix <felix.sheldon@industry.nsw.gov.au>
2020-01-11 22:36:59 +1100

    aem points to hdf for faster loading

--- points_to_hdf5.py
+++ points_to_hdf5.py
@@ -1,16 +1,24 @@
 import numpy as np
 import h5py
 
+from read_aem_points import load_aem_points
 
-d1 = np.random.random(size=(1000, 20))
-d2 = np.random.random(size=(1000, 200))
+
+data_points, lines = load_aem_points()
+
+dp = np.array(data_points)
 
 with h5py.File('data/data.h5', 'w') as hf:
-    hf.create_dataset('dataset_1', data=d1)
-    hf.create_dataset('dataset_2', data=d2)
+    hf.create_dataset('nt_aem_pts', data=dp, compression="gzip", compression_opts=9)
 
 
+hf = h5py.File('data/data.h5', 'r')
+print(hf.keys())
 
+pts = hf.get('nt_aem_pts')
+print(pts)
 
+pts_arr = np.array(pts)
+print(pts_arr.shape)
 
-
+hf.close()
--- read_aem_points.py
+++ read_aem_points.py
@@ -2,25 +2,26 @@
 # DEFN 11 ST=RECD,RT=;Longitude:f13.7:UNIT=deg:NULL=-999.9999999,NAME=Longitude:DATUM=GDA94
 
 
-data_points = []
-lines = set()
+def load_aem_points():
 
+    data_points = []
+    lines = set()
 
-with open("data/AusAEM_Year1_NT_Final_CND.dat") as nt_aem_file:
+    with open("data/AusAEM_Year1_NT_Final_CND.dat") as nt_aem_file:
 
-    for l in nt_aem_file.readlines():
-        toks = l.split()
-        line = int(toks[0])
-        lat = float(toks[10])
-        lon = float(toks[11])
+        for l in nt_aem_file.readlines():
+            toks = l.split()
+            line = int(toks[0])
+            lat = float(toks[10])
+            lon = float(toks[11])
 
-        pt = (line, lon, lat)
-        data_points.append(pt)
+            pt = (line, lon, lat)
+            data_points.append(pt)
 
-        lines.add(line)
+            lines.add(line)
 
-print(len(data_points))
-print(len(lines))
+    print(len(data_points))
+    print(len(lines))
 
-
+    return data_points, lines
 
