38ed3102cdcf27fff3fe9be4a1c395974a49b0c7
felix <felix.sheldon@industry.nsw.gov.au>
2020-01-12 17:40:45 +1100

    normalise c.d. interval values

--- points_normalise.py
+++ points_normalise.py
@@ -15,5 +15,22 @@ hf.close()
 print(pts_arr.shape)
 
 
+rows_max = []
+
+for row in pts_arr:
+    row_max = max(row[3:])
+    rows_max.append(row_max)
+
+max_value = max(rows_max)
+print(max_value)
+
+# print(pts_arr[:20, 3:] / max_value)
+
+normalised_values = pts_arr[:, 3:] / max_value
+
+with h5py.File('data/data.h5', 'a') as hf:
+    hf.create_dataset('nt_aem_pts_normalised', data=normalised_values, compression="gzip", compression_opts=9)
+
+
 
 
